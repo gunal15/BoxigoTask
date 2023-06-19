2d83bff2787fdab2edd18bf99a4b7492171bca92
gunal15 <gunalnandakumar@gmail.com>
2023-06-19 10:55:47 +0530

    css

--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -4,7 +4,14 @@ const Logout = () => {
   return (
     <div>
 
-<h1 className='display-3 text-center'>Logout Page</h1>
+<h1   className="display-3 text-center"
+  style={{
+    fontSize: '3rem',
+    fontWeight: 'bold',
+    color: '#333',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
+    marginTop: '50px',
+  }}>Logout Page</h1>
 
     </div>
   )
--- src/Components/MyProfile.js
+++ src/Components/MyProfile.js
@@ -3,7 +3,19 @@ import React from 'react'
 const MyProfile = () => {
   return (
     <div>
-     <h1 className='display-3 text-center'>Profile Page</h1>
+     <h1
+  className="display-3 text-center"
+  style={{
+    fontSize: '3rem',
+    fontWeight: 'bold',
+    color: '#333',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
+    marginTop: '50px',
+  }}
+>
+  Profile Page
+</h1>
+
     </div>
   )
 }
